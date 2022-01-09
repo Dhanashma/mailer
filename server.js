7b777ef620b4d04a8f0a21ddb8c7d344b72bd51c
dhanashma <dhanashma@gmail.com>
2022-01-09 17:21:10 +0530

    test

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app =express();
-const port = 8082;
+const PORT = 3000;
 const path = require('path')
 const nodemailer = require('nodemailer');
 
 
-app.listen(port,()=>{
-    console.log('code is ready in port',8082);
+app.listen(PORT,()=>{
+    console.log('code is ready in port',3000);
 })
 
 app.use(express.urlencoded({extended:false}));
